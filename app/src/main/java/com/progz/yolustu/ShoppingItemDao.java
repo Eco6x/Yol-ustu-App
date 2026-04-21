dd0963aaa3ebc148899896fcdf1f134e8722f0a9
Ammar <230044653+Eco6x@users.noreply.github.com>
2026-04-21 21:38:43 +0300

    Update ShoppingItemDao.java
    added the command to delete an item

--- app/src/main/java/com/progz/yolustu/ShoppingItemDao.java
+++ app/src/main/java/com/progz/yolustu/ShoppingItemDao.java
@@ -1,8 +1,10 @@
 package com.progz.yolustu;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 import java.util.List;
 
 @Dao
@@ -11,7 +13,15 @@ public interface ShoppingItemDao {
     @Insert
     void insert(ShoppingItem item);
 
-    // Command to read the list
+    // Command to update an item (used when Ammar clicks the checkbox)
+    @Update
+    void update(ShoppingItem item);
+
+    // Command to delete an item
+    @Delete
+    void delete(ShoppingItem item);
+
+    // Command to read the whole list
     @Query("SELECT * FROM shopping_items")
     List<ShoppingItem> getAllItems();
 }
